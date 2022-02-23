6a15d4c68e1a27fe33093a14556239ac5c7be6f1
chenjingdev <chenjingdev@gmail.com>
2022-02-23 18:04:26 +0900

    save point

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from "react";
+
 function App() {
+  const [toDo, setToDo] = useState("");
+  const onChange = (event) => setToDo(event.target.value);
+  // const onSubmit = (event) => event.preventDefault();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input onChange={onChange} type={toDo} placeholder="Write your to do...." />
+        <button>addbtn</button>
+      </form>
     </div>
   );
 }
